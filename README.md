e0fe4cf343b44bdea89c6faa8c344cb885ef26de
g1.kwon <spring_shot@naver.com>
2021-10-26 18:15:49 +0900

    단순한 API 라우팅 처리와 HTTPie를 이용한 테스팅

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 nodeapi without express
 
 # nodemon
+npm install --save-dev nodempn
+
+#test
+brew install httpie
--- src/main.js
+++ src/main.js
@@ -8,3 +8,29 @@
  * 인증 로직은 넣지 않는다.
  * RESTful API를 사용한다.
  */
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/posts' && req.method === 'GET') {
+        res.statusCode = 200
+        res.end('List of posts!')
+    } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+        res.statusCode = 200
+        res.end('Come content of the post')
+    } else if (req.url === '/posts' && req.method === 'POST') {
+        res.statusCode = 200
+        res.end('Creating post')
+    } else {
+        res.statusCode = 404
+        res.end('Not found.')
+    }
+
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+    console.log(`The server is listening at port: ${PORT}`)
+})
