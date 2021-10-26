5e5a65e508c290df98644d2715aa9292e245e777
g1.kwon <spring_shot@naver.com>
2021-10-27 01:08:43 +0900

    가독성과 유지보수성을 살리는 리팩토링 2

--- src/api.js
+++ src/api.js
@@ -31,7 +31,7 @@ const posts = [
  * @typedef Route
  * @property {RegExp} url
  * @property {'GET' | 'POST'} method
- * @property {() => promise<APIResponse>} callback
+ * @property {(matches: string[], body: Object.<string, *> | undefined) => promise<APIResponse>} callback
  * */
 
 /** @type {Route[]} */
@@ -40,28 +40,65 @@ const routes = [
         url: /^\/posts$/,
         method: 'GET',
         callback: async () => ({
-            // TODO: implement
             statusCode: 200,
-            body: {},
+            body: posts,
         }),
     },
     {
         url: /^\/posts\/([a-zA-Z0-9-_]+)$/,
         method: 'GET',
-        callback: async () => ({
-            // TODO: implement
-            statusCode: 200,
-            body: {},
-        }),
+        callback: async (matches) => {
+            const postId = matches[1]
+            if (!postId) {
+                return {
+                    statusCode: 404,
+                    body: 'Not found',
+                }
+            }
+
+            const post = posts.find(_post => _post.id === postId)
+
+            if (!post) {
+                return {
+                    statusCode: 404,
+                    body: 'Not found',
+                }
+            }
+
+            return {
+                statusCode: 200,
+                body: posts,
+            }
+        },
     },
     {
         url: /^\/posts$/,
         method: 'POST',
-        callback: async () => ({
-            // TODO: implement
-            statusCode: 200,
-            body: {},
-        }),
+        callback: async (_, body) => {
+
+            if (!body) {
+                return {
+                    statusCode: 400,
+                    body: 'Ill-formed request.'
+                }
+            }
+
+            /** @type {string} */
+            /* eslint-disable-next-lint prefer-destructuring */
+            const title = body.title
+            const newPost = {
+                id: title.replace(/\s/g, '_'),
+                title,
+                content: body.content
+            }
+
+            posts.push(newPost)
+
+            return {
+                statusCode: 200,
+                body: newPost,
+            }
+        },
     }
 ]
 
